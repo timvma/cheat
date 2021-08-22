1c44cd1d382a714efb0cf3bfec8c6eebf0cbc443
Tim <timawbi@gmail.com>
2021-08-22 10:32:55 +0900

    Vue3

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/css',
+    name: 'Css',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Css.vue')
+  },
+  {
+    path: '/javascript',
+    name: 'Javascript',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Javascript.vue')
   }
 ]
 
--- src/views/Css.vue
+++ src/views/Css.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">    
+    CSS
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
+  name: 'Css',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
